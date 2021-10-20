53f163579ba02fca6c6f72cf9c3f450ce3b0a9a7
clairefro <claire.froelich@gmail.com>
2021-10-19 17:23:32 -0700

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <h1 className="text-center my-24 font-black tracking-tight text-6xl">
+      Our homepage
+    </h1>
+  );
+}
